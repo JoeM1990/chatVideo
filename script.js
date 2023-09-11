8093da2c766fc76f49744c84c8530f5f49ce487e
Joe Monkila <jnkiwa25@gmail.com>
2023-09-11 14:52:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+const REMOTE = document.getElementById('remote');
+
+let idUser;
+let conn;
+
+  var peer = new Peer('pc2Monkila', {
+    secure:true,
+    host: '',
+  }
+  );
+
+  peer.on('open', function () {
+   console.log(peer.id)
+  });
+
+  peer.on('connection', function(c) {
+
+    if (conn && conn.open) {
+        c.on('open', function() {
+            c.send("Already connected to another client");
+            setTimeout(function() { c.close(); }, 500);
+        });
+        return;
+    }
+
+    
+    conn = c;
+    console.log("Connected to: " + conn.peer);
+
+    conn.on('data', function(data){
+      console.log(data);
+    });
+  });
+
+
+  const constraints = {
+    video: true,
+    width: 640,
+    height: 480
+  };
+
+  peer.on('call', function(call) {
+
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        call.answer(stream); 
+        call.on('stream', function(remoteStream) {
+            REMOTE.srcObject = remoteStream
+        });
+    });
+
+});
+
+
+  
+  
